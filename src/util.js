19bd727854e8c94c590a3b7531e97d3a5b3efe18
nguyen199801 <nguyennc710@gmail.com>
2021-07-26 15:44:43 +0700

    fix

--- src/util.js
+++ src/util.js
@@ -12,13 +12,13 @@ const casesTypeColors = {
         hex: "#CC1034",
         rgb: "rgb(204, 16, 52)",
         half_op: "rgba(204, 16, 52, 0.5)",
-        multiplier: 800,
+        multiplier: 400,
     },
     recovered: {
         hex: "#7dd71d",
         rgb: "rgb(125, 215, 29)",
         half_op: "rgba(125, 215, 29, 0.5)",
-        multiplier: 1200,
+        multiplier: 500,
     },
     deaths: {
         hex: "#fb4443",
